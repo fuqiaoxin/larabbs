12d38e097387dc86c05b710e52288c493bdf0cff
vagrant <vagrant@homestead>
2018-01-05 08:06:54 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,23 @@
 
 Route::get('/','PagesController@root')->name('root');
 
+
+// Auth::routes();  此路由等同下面 用户认证的路由 Authentication Routes,Registration Routes,Password Reset Routes
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+
+
+//Route::get('/home', 'HomeController@index')->name('home');    // 主页路由上面已经有了，此处就不需要了
