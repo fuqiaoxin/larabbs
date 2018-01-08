07f34e1492f54650d0046e8c7d6d0a3bb7740eb5
vagrant <vagrant@homestead>
2018-01-08 10:04:01 +0000

    Basic user profile editing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
         @include('layouts._header')
 
         <div class="container">
+            @include('layouts._message')
             @yield('content')
         </div>
         @include('layouts._footer')
