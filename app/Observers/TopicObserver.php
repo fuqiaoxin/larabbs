5e5c0e49870c258aa2627b7b84391093441a93ae
vagrant <vagrant@homestead>
2018-01-10 10:24:00 +0000

    prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,8 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        // 根据过滤规则过滤body字段中非法字符 config/purifier.php user_topic_body
+        $topic->body = clean($topic->body,'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 }
