065e2fe490bc66247a81d02d9dcea7cca0947d8b
vagrant <vagrant@homestead>
2018-04-17 05:38:23 +0000

    User can create reply

--- app/Http/Requests/ReplyRequest.php
+++ app/Http/Requests/ReplyRequest.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Requests;
 
+/**
+ * Class ReplyRequest
+ * @package App\Http\Requests
+ * @property string $content
+ * @property string $topic_id
+ * @property string $user_id
+ */
 class ReplyRequest extends Request
 {
     public function rules()
@@ -13,6 +20,7 @@ class ReplyRequest extends Request
             {
                 return [
                     // CREATE ROLES
+                    'content' => 'required|min:2'
                 ];
             }
             // UPDATE
