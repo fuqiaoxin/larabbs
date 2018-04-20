2686585797f8d57d4087d687df30b5166eed7407
vagrant <vagrant@homestead>
2018-04-20 08:16:10 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/','TopicsController@index')->name('root');
 
 
 // Auth::routes();  此路由等同下面 用户认证的路由 Authentication Routes,Registration Routes,Password Reset Routes
