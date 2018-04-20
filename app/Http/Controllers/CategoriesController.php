e0c95cf3e5eb2c719f8ac2a3eda6cde97eb8ef20
vagrant <vagrant@homestead>
2018-04-20 03:30:11 +0000

    Siderbar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,15 +6,18 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Topic $topic,Request $request,User $user){
-        $active_users = $user->getActiveUsers();
+    public function show(Category $category,Topic $topic,Request $request,User $user, Link $link){
 
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
-        return view('topics.index',compact('topics','category','active_users'));
+
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
+        return view('topics.index',compact('topics','category','active_users','links'));
 
     }
 }
--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -18,12 +19,13 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic, User $user)
+	public function index(Request $request, Topic $topic, User $user, Link $link)
 	{
-        $active_users = $user->getActiveUsers();
-
 		$topics = $topic->withOrder($request->order)->paginate(30);
-		return view('topics.index', compact('topics','active_users'));
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
+
+		return view('topics.index', compact('topics','active_users','links'));
 	}
 
     public function show(Request $request,Topic $topic)
